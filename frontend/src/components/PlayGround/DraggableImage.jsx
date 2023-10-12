ec0dd381d128aff342caa86b15fa5671257f63b3
ZAWADBINSHARIF <syedzawadbinsharif@gmail.com>
2023-10-12 08:54:04 +0600

    Context menu of Draggable Image has been designed

--- frontend/src/components/PlayGround/DraggableImage.jsx
+++ frontend/src/components/PlayGround/DraggableImage.jsx
@@ -1,16 +1,36 @@
 // external import
 import { useDraggable } from "@dnd-kit/core"
 import { CSS } from "@dnd-kit/utilities"
+import { useState } from "react"
+import { useDispatch } from "react-redux"
 
 // internal import
+import { removeDraggableImg } from "../../Store/Slices/DraggableImgSlice.js"
+import { getRemovedDraggableImg } from "../../Store/Slices/RemovedDraggableImgSlice.js"
 
-const DraggableImage = ({ id, src, x, y, name, role, folder_name, draggable_id }) => {
+const DraggableImage = ({
+    id,
+    src,
+    x,
+    y,
+    name,
+    role,
+    folder_name,
+    draggable_id }) => {
 
     const apiPath = import.meta.env.VITE_API
-
-    const { attributes, listeners, setNodeRef, transform, isDragging } = useDraggable({
+    const [showContextMenu, setShowContextMenu] = useState(false)
+    const dispatch = useDispatch()
+    const {
+        attributes,
+        listeners,
+        setNodeRef,
+        transform,
+        isDragging
+    } = useDraggable({
         id: draggable_id
     })
+    let timer;
 
     const style = {
         transform: CSS.Translate.toString(transform),
@@ -20,23 +40,63 @@ const DraggableImage = ({ id, src, x, y, name, role, folder_name, draggable_id }
         position: 'absolute'
     }
 
+    function handleRemove() {
+        dispatch(getRemovedDraggableImg(draggable_id))
+        dispatch(removeDraggableImg(draggable_id))
+    }
+
+    function ContextMenu() {
+        return (
+            <div className="ContextMenu">
+                <span>Peanut Gallery</span>
+                <span
+                    onClick={() => handleRemove()}
+                    className="text-danger">
+                    Leave
+                </span>
+            </div>
+        )
+    }
+
+    function handleContextMenu(e) {
+        e.preventDefault()
+        setShowContextMenu(prev => !prev)
+    }
+
+    function handleOnTouchStart() {
+        timer = setTimeout(handleContextMenu, 500)
+    }
+
+    function handleOnTouchEnd() {
+        if (timer)
+            clearTimeout(timer)
+    }
+
     return (
         <div
             className="DraggableImage"
-            ref={setNodeRef}
+            onClick={() => setShowContextMenu(false)}
+            onTouchStart={() => handleOnTouchStart()}
+            onTouchEnd={() => handleOnTouchEnd()}
             style={style}
-            {...attributes}
-            {...listeners}
+            ref={setNodeRef}
+            onContextMenu={handleContextMenu}
         >
-            <img src={`${apiPath}/${folder_name}/${src}`} />
+            {showContextMenu && <ContextMenu />}
 
+            {/* // ! do not remove this div. If you do, any click event will not work*/}
+            <div
+                {...attributes}
+                {...listeners}
+            >
+                <img src={`${apiPath}/${folder_name}/${src}`} />
 
-            <div className="draggableCharInfo mt-2">
-                {`${name}`}
-                <br />
-                {`${role}`}
+                <div className="draggableCharInfo mt-2">
+                    {`${name}`}
+                    <br />
+                    {`${role}`}
+                </div>
             </div>
-
         </div>
     )
 }
